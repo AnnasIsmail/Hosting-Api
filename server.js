7a7158329d320a0dca080244310ab2f2ea7dcf20
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2022-08-24 23:21:40 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ const server = http.createServer((req , res) => {
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server Running at https://${hostname}:${port}`);
+    console.log(`Server Running at https://${hostname}port:${port}`);
 });
